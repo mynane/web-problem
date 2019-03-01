96058a9651cfe1f11c9c49dadec48f0c4aebce90
石进华 <mxj@MacBook-Pro-7.local>
2019-03-01 13:41:25 +0800

    merge

--- build/index.js
+++ build/index.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var path = require('path');//解析需要遍历的文件夹
 var filePath = path.resolve('../');
-var template = '# web-problem\n\n记录学习中遇到的问题，记录成长\n| 期数   | 主题 | 地址 |\n| :-----: | :------: | :------: |'
+var template = '# web-problem\n记录学习中遇到的问题，记录成长\n\n| 期数   | 主题 | 地址 |\n| :-----: | :------: | :------: |'
 var ignoreFiles = ['README.md', '.gitignore']
 
 //调用文件遍历方法
