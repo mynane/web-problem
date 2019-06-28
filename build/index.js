2165ffbd65ee30518f7c9b3aeab0d21ae0799f35
石进华 <shijinhua@shijinhuadeMacBook-Pro.local>
2019-06-28 10:26:24 +0800

    README自动生成:Fri Jun 28 2019 10:26:24 GMT+0800 (GMT+08:00)

--- build/index.js
+++ build/index.js
@@ -3,18 +3,21 @@ const process = require('child_process');
 const path = require('path');
 const { fileDisplay } = require('./run');
 
-const filePath = path.resolve('../docs/');
+const filePath = path.resolve('./docs/');
 
 function run() {
-  fileDisplay(filePath);
   const date = new Date();
-
-  process.exec(`cd .. && git add . && git commit -m  'README自动生成:${date}' && git pull && git push`, function (error, stdout, stderr) {
-    console.log(stderr);
-    if (error !== null) {
-      console.log('exec error: ' + error);
+  process.exec(`cd .. && git add . && git commit -m  'README自动生成:${date}' && git pull`, function (error, stdout, stderr) {
+    if (error != null) {
+      fileDisplay(filePath);
+      process.exec(`cd .. && git add . && git commit --amend' && git pull && git push`, function (error, stdout, stderr) {
+        console.log(stderr);
+        if (error !== null) {
+          console.log('exec error: ' + error);
+        }
+      });
     }
-  });
+  })
 }
 
 const  scheduleCronstyle = ()=>{
