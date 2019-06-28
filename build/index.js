df875dbc8a6ab719bfe1b53bbb3ed25b20cfb2e9
石进华 <shijinhua@shijinhuadeMacBook-Pro.local>
2019-06-28 10:39:45 +0800

    README自动生成:Fri Jun 28 2019 10:39:45 GMT+0800 (GMT+08:00)

--- build/index.js
+++ build/index.js
@@ -6,15 +6,30 @@ const { fileDisplay } = require('./run');
 const filePath = path.resolve('../docs/');
 
 function run() {
-  fileDisplay(filePath);
-  const date = new Date();
+  process.exec(`cd .. && git pull && git add .`, function (error, stdout, stderr) {
+    console.log(error);
+    if (error === null) {
+      fileDisplay(filePath);
+      const date = new Date();
 
-  process.exec(`cd .. && git add . && git commit -m  'README自动生成:${date}' && git push`, function (error, stdout, stderr) {
-    console.log(stderr);
-    if (error !== null) {
-      console.log('exec error: ' + error);
+      process.exec(`cd .. && git add . && git commit -m  'README自动生成:${date}' && git push`, function (error, stdout, stderr) {
+        console.log(stderr);
+        if (error !== null) {
+          console.log('exec error: ' + error);
+        }
+      });
     }
-  });
+  })
+
+  // fileDisplay(filePath);
+  // const date = new Date();
+
+  // process.exec(`cd .. && git add . && git commit -m  'README自动生成:${date}' && git push`, function (error, stdout, stderr) {
+  //   console.log(stderr);
+  //   if (error !== null) {
+  //     console.log('exec error: ' + error);
+  //   }
+  // });
 }
 
 const  scheduleCronstyle = ()=>{
